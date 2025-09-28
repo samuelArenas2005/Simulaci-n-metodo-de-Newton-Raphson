7c2aa6435cd52de518bc81050475b123ebc60c2a
Danicol12 <Danicol12>
2025-09-27 23:45:59 -0500

    Se creo function y se comprimieron todas las funciones en vector

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from vector import Vector
-from jacobiano import Jacobiano
+
 
 def main():
     
     xinit= Vector(729)
     xinit.vecInicial()
-    xinit.showPlot()
+    xinit.showPlot(True)
+    xinit.cal_jacobiano()
+    xinit.cal_inv_jacobiano()
+    xinit.getfunction(1)
+    xinit.showInConsoleFunction(True)
     
-    jacobiano = Jacobiano(729)
-    jacobiano.cal_jacobiano(xinit.vec)
+
+
+   
     
     #si quieres crear un nuevo archivo excel para ver la matriz pon show=True en los parametros xd
-    jacobiano.showMatrixJacobiana() 
-    
-    jacobiano.cal_inv_jacobiano()
-    jacobiano.showMatrixJacobiana()
-    
+ 
 
 if __name__ == "__main__":
     main()
