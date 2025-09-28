89b1a912a56c7659c8c07ab4869bd83a5c3cd46a
SamuelArenas2005 <e.sir.samuel.arenas@cali.edu.co>
2025-09-28 13:32:50 -0500

    arreglo de bugs

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from vector import Vector
 
 def main():
     xinit= Vector(729)
-    xinit.vecInicial(20)
-    for i in range(0, 2):
+    xinit.vecInicial()
+    
+    for i in range(0, 8):
         xinit.cal_jacobiano()
         xinit.cal_inv_jacobiano()
-        xinit.cal_function(20)
+        xinit.cal_function()
         xinit.newVector()
+    
     xinit.showPlot(True)
+        
+    
+   
     
 
 if __name__ == "__main__":
--- vector.py
+++ vector.py
@@ -6,14 +6,16 @@ import matplotlib.pyplot as plt
 class Vector:
 
     def __init__(self,n):
-        self.vec = np.array([0]*n) 
-        self.vectFunction = np.array([0]*n) 
+        self.vec = np.zeros(n)
+        self.vectFunction = np.zeros(n)
         self.matrixJacobiana = np.zeros((n,n))
-    
+                    
+
     def vecInicial(self,velocidad_init=1):
         V0 = velocidad_init
         k = 0
-        rango = [20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 1, 0]
+        """ rango = [20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 1, 0] """
+        rango = [0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.05,0]
         x0 = self.vec
         for i in range (0, 81): 
             if (i % 10 == 9 and rango[k+1] != 0.1): k += 1
@@ -28,6 +30,7 @@ class Vector:
                     x0[j*81 + i] = random.uniform(rango[k+2], rango[k+3])
                 else:
                     x0[j*81 + i] = random.uniform(rango[k], rango[k+1])
+        
 
     def vecInicialUniform(self,velocidad_init=1):
         V0 = velocidad_init
@@ -42,9 +45,9 @@ class Vector:
                     x0[j*81 + i] = 1
 
 
-    def cal_function(self, V0=1):
+    def cal_function(self):
         
-        val_Vij=0.2
+        val_Vij=0.5
         x0=self.vec
 
         for i in range (0, 81): 
@@ -72,8 +75,8 @@ class Vector:
                     
     def newVector(self):
         xn = np.subtract(self.vec, np.matmul(self.matrixJacobiana, self.vectFunction))
+        
         self.vec = xn
-        return xn
 
     def showInConsole(self,condicion=False):
         x0 = self.vec
@@ -84,7 +87,7 @@ class Vector:
                 print(fila_str)
                 
                 
-    def showPlot(self,condicion=True):
+    def showPlot(self,condicion=False):
         if condicion:
             x0 = self.vec
                 # --- visualización ---
@@ -94,11 +97,10 @@ class Vector:
                 # crear un mapa de calor
             plt.figure(figsize=(15, 3))
             cmap = plt.cm.viridis  # colormap principal
-            cmap.set_under('gray')  # valores menores al vmin → gris
             # valores menores al vmin → gris
 
             # plot con imshow
-            im = plt.imshow(matriz[::-1], cmap=cmap, vmin=0.0001, vmax=20)  
+            im = plt.imshow(matriz[::-1], cmap=cmap, vmin=0.0001, vmax=1)  
             # matriz[::-1] → para que se vea de abajo hacia arriba (como ejes cartesianos)
 
             plt.colorbar(im, label="u valores")
@@ -107,16 +109,17 @@ class Vector:
             plt.ylabel("j (y)")
             plt.show()
 
-
     def cal_jacobiano(self):
         
-        v_ij= 0.2
+        v_ij= 0.5
 
         vec_0=self.vec
         
+        self.matrixJacobiana.fill(0.0)
+        
         valores_derechos = [ecuacion * 81 - 1  for ecuacion in range(1, 10)]
-        valores_bloque_A = [j * 81 + k for j in range(11,20) for k in range(0,3)]
-        valores_bloque_B = [j * 81 + k for j in range(36,58) for k in range(5,8)]
+        valores_bloque_A = [k * 81 + j for j in range(11,20) for k in range(0,3)]
+        valores_bloque_B = [k * 81 + j for j in range(36,58) for k in range(5,8)]
         
         for ecuacion in range (729):
             if ((ecuacion % 81 == 0) or (ecuacion >= 0 and ecuacion<=81) or (ecuacion >= 81*8  and ecuacion<=728) or ecuacion in valores_derechos 
@@ -128,7 +131,7 @@ class Vector:
                 #aqui van las derivadas parciales, en la posición [i,i] [i,i+1] [i,i-1].... Tal y como lo definimos
                 i = ecuacion #Por comodidad visual 
                 
-                self.matrixJacobiana[ecuacion,i] = (-5/8)*(vec_0[i+1]-vec_0[i-1])
+                self.matrixJacobiana[ecuacion,i] = (-5/8)*(vec_0[i+1]-vec_0[i-1])-1
                 
                 self.matrixJacobiana[ecuacion,i+1] = (1/4) - (5/8)*vec_0[i]
                 
